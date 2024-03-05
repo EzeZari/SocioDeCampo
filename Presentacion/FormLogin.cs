2c4d17b6c753e41e1701153eb2e11a6157f51b81
EzeZari <Ezequielzari@hotmail.com>
2024-03-05 17:09:36 -0300

    Verificación de existencia de usuario

--- Presentacion/FormLogin.cs
+++ Presentacion/FormLogin.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Domain;
 
 namespace Presentacion
 {
@@ -22,7 +23,24 @@ namespace Presentacion
             
                 if (txtuser.Text != "Username")
                 {
-                    if (txtpassword.Text != "Password") { }
+                    if (txtpassword.Text != "Password") {
+                    UserModel user = new UserModel();
+                    var validLogin = user.LoginUser(txtuser.Text, txtpassword.Text);
+                    if(validLogin == true)//Si el inicio de sesion es verdadero mostramos el form principal
+                    {
+                        FormPrincipal mainMenu = new FormPrincipal();
+                        mainMenu.Show();  //Mostramos el form principal
+                        this.Hide();     //Ocultamos el Login
+                    }
+                    else
+                    {
+                        msgError("El usuario o contraseña no son correctos");
+                        txtuser.Text = ""; //Limpiamos el txt user 
+                        txtpassword.Focus();
+                    }
+                
+                
+                }
                     else msgError("Por favot ingresa tu contraseña");
                 }
                 else msgError(" Por favor ingresa tu Usuario");
