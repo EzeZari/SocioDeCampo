ae84cb7e89ad2235cbc6878a4e9aad18f26a57c1
EzeZari <103504490+EzeZari@users.noreply.github.com>
2025-07-23 12:30:55 -0300

    Agregue Fom de detealles del partido

--- Presentacion/Form Partidos/FormVerDetallePartido.Designer.cs
+++ Presentacion/Form Partidos/FormVerDetallePartido.Designer.cs
@@ -29,12 +29,134 @@ namespace Presentacion
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormVerDetallePartido));
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lblHora = new System.Windows.Forms.Label();
+            this.lblLocal = new System.Windows.Forms.Label();
+            this.lblVisitante = new System.Windows.Forms.Label();
+            this.lblResultadoLocal = new System.Windows.Forms.Label();
+            this.lblResultadoVisitante = new System.Windows.Forms.Label();
+            this.lblObservaciones = new System.Windows.Forms.Label();
+            this.btnCerrar = new Guna.UI.WinForms.GunaButton();
+            this.SuspendLayout();
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(45, 80);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(35, 13);
+            this.lblFecha.TabIndex = 0;
+            this.lblFecha.Text = "label1";
+            // 
+            // lblHora
+            // 
+            this.lblHora.AutoSize = true;
+            this.lblHora.Location = new System.Drawing.Point(50, 147);
+            this.lblHora.Name = "lblHora";
+            this.lblHora.Size = new System.Drawing.Size(35, 13);
+            this.lblHora.TabIndex = 1;
+            this.lblHora.Text = "label1";
+            // 
+            // lblLocal
+            // 
+            this.lblLocal.AutoSize = true;
+            this.lblLocal.Location = new System.Drawing.Point(50, 195);
+            this.lblLocal.Name = "lblLocal";
+            this.lblLocal.Size = new System.Drawing.Size(35, 13);
+            this.lblLocal.TabIndex = 2;
+            this.lblLocal.Text = "label1";
+            // 
+            // lblVisitante
+            // 
+            this.lblVisitante.AutoSize = true;
+            this.lblVisitante.Location = new System.Drawing.Point(50, 238);
+            this.lblVisitante.Name = "lblVisitante";
+            this.lblVisitante.Size = new System.Drawing.Size(35, 13);
+            this.lblVisitante.TabIndex = 3;
+            this.lblVisitante.Text = "label1";
+            // 
+            // lblResultadoLocal
+            // 
+            this.lblResultadoLocal.AutoSize = true;
+            this.lblResultadoLocal.Location = new System.Drawing.Point(50, 288);
+            this.lblResultadoLocal.Name = "lblResultadoLocal";
+            this.lblResultadoLocal.Size = new System.Drawing.Size(35, 13);
+            this.lblResultadoLocal.TabIndex = 4;
+            this.lblResultadoLocal.Text = "label1";
+            // 
+            // lblResultadoVisitante
+            // 
+            this.lblResultadoVisitante.AutoSize = true;
+            this.lblResultadoVisitante.Location = new System.Drawing.Point(50, 327);
+            this.lblResultadoVisitante.Name = "lblResultadoVisitante";
+            this.lblResultadoVisitante.Size = new System.Drawing.Size(35, 13);
+            this.lblResultadoVisitante.TabIndex = 5;
+            this.lblResultadoVisitante.Text = "label1";
+            // 
+            // lblObservaciones
+            // 
+            this.lblObservaciones.AutoSize = true;
+            this.lblObservaciones.Location = new System.Drawing.Point(50, 376);
+            this.lblObservaciones.Name = "lblObservaciones";
+            this.lblObservaciones.Size = new System.Drawing.Size(35, 13);
+            this.lblObservaciones.TabIndex = 6;
+            this.lblObservaciones.Text = "label1";
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.AnimationHoverSpeed = 0.07F;
+            this.btnCerrar.AnimationSpeed = 0.03F;
+            this.btnCerrar.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.btnCerrar.BorderColor = System.Drawing.Color.Black;
+            this.btnCerrar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnCerrar.FocusedColor = System.Drawing.Color.Empty;
+            this.btnCerrar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnCerrar.ForeColor = System.Drawing.Color.White;
+            this.btnCerrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCerrar.Image")));
+            this.btnCerrar.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnCerrar.Location = new System.Drawing.Point(285, 309);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnCerrar.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnCerrar.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnCerrar.OnHoverImage = null;
+            this.btnCerrar.OnPressedColor = System.Drawing.Color.Black;
+            this.btnCerrar.Size = new System.Drawing.Size(160, 42);
+            this.btnCerrar.TabIndex = 7;
+            this.btnCerrar.Text = "gunaButton1";
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // FormVerDetallePartido
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.lblObservaciones);
+            this.Controls.Add(this.lblResultadoVisitante);
+            this.Controls.Add(this.lblResultadoLocal);
+            this.Controls.Add(this.lblVisitante);
+            this.Controls.Add(this.lblLocal);
+            this.Controls.Add(this.lblHora);
+            this.Controls.Add(this.lblFecha);
+            this.Name = "FormVerDetallePartido";
             this.Text = "FormVerDetallePartido";
+            this.Load += new System.EventHandler(this.FormVerDetallePartido_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label lblHora;
+        private System.Windows.Forms.Label lblLocal;
+        private System.Windows.Forms.Label lblVisitante;
+        private System.Windows.Forms.Label lblResultadoLocal;
+        private System.Windows.Forms.Label lblResultadoVisitante;
+        private System.Windows.Forms.Label lblObservaciones;
+        private Guna.UI.WinForms.GunaButton btnCerrar;
     }
 }
--- Presentacion/Form Partidos/FormVerDetallePartido.cs
+++ Presentacion/Form Partidos/FormVerDetallePartido.cs
@@ -20,5 +20,22 @@ namespace Presentacion
             InitializeComponent();
             this.partido = partido;
         }
+
+        private void FormVerDetallePartido_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = partido.Fecha.ToShortDateString();
+            lblHora.Text = partido.Hora.ToString(@"hh\:mm");
+            lblLocal.Text = partido.EquipoLocal;
+            lblVisitante.Text = partido.EquipoVisitante;
+
+            lblResultadoLocal.Text = partido.ResultadoLocal.ToString();
+            lblResultadoVisitante.Text = partido.ResultadoVisitante.ToString();
+            lblObservaciones.Text = partido.Observaciones;
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
